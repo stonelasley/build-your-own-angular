84a5167aa633e9cbd7e5ff7e6d998aa4aac137b2
Stone C. Lasley <stonelasley@gmail.com>
2016-01-05 22:37:59 -0700

    Working on  and

--- src/hello.js
+++ src/hello.js
@@ -2,4 +2,4 @@ var _ = require('lodash');
 
 module.exports = function sayhello(to) {
   return _.template("Hello, <%= name %>!")({name: to});
-}
+};
---
+++ src/scope.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var _ = require('lodash');
+
+function Scope() {
+  this.$$watchers = [];
+}
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn
+  };
+
+  this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function() {
+  var self = this;
+  _.forEach(this.$$watchers, function(watcher) {
+    watcher.watchFn(self);
+    watcher.listenerFn();
+  });
+};
+
+module.exports = Scope;
