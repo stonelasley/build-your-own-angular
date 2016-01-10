aa29173660db0894cd79a7c16416a2b1dbe682c4
Stone C. Lasley <stonelasley@gmail.com>
2016-01-10 12:01:00 -0700

    Watchgroup passes new vals as old on first run

--- src/scope.js
+++ src/scope.js
@@ -131,6 +131,38 @@ Scope.prototype.$watch = function (watchFn, listenerFn, valueEq) {
   };
 };
 
+Scope.prototype.$watchGroup = function (watchFns, listenerFn) {
+  var self = this;
+
+  var newValues = new Array(watchFns.length);
+  var oldValues = new Array(watchFns.length);
+  var changeReactionScheduled = false;
+  var firstRun = true;
+
+  function watchGroupListener() {
+    if(firstRun){
+      firstRun = false;
+      listenerFn(newValues, newValues, self);
+    } else {
+      listenerFn(newValues, oldValues, self);
+    }
+
+    changeReactionScheduled = false;
+  }
+
+  _.forEach(watchFns, function (watchFn, i){
+    self.$watch(watchFn, function(newValue, oldValue){
+      newValues[i] = newValue;
+      oldValues[i] = oldValue;
+
+      if(!changeReactionScheduled) {
+        changeReactionScheduled = true;
+        self.$evalAsync(watchGroupListener);
+      }
+    });
+  });
+};
+
 Scope.prototype.$$areEqual = function (newValue, oldValue, valueEq) {
 
   if (valueEq) {
