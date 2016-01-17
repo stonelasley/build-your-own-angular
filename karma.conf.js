c8086ff172b0d57783de348e3ae74d79f866a1ee
Stone C. Lasley <stonelasley@gmail.com>
2016-01-16 17:41:56 -0700

    Code Coverage and Coveralls Reporting

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,4 @@
+var istanbul = require('browserify-istanbul');
 module.exports = function(config) {
   config.set({
     frameworks: ['browserify', 'jasmine'],
@@ -7,11 +8,25 @@ module.exports = function(config) {
     ],
     preprocessors: {
       'test/**/*.js': ['jshint', 'browserify'],
-      'src/**/*.js': ['jshint', 'browserify']
+      'src/**/*.js': ['jshint', 'browserify', 'coverage']
     },
     browsers: ['PhantomJS'],
     browserify: {
       debug: true
+    },
+    reporters: ['progress', 'coverage', 'coveralls'],
+    browserify: {
+      debug: true,
+      transform: [istanbul({
+        ignore: ['**/node_modules/**', '**/test/**'],
+      })],
+    },
+    coverageReporter: {
+      dir: 'coverage/',
+      reporters: [
+        {type: 'lcov', subdir: 'report-lcov'},
+        {type: 'text-summary'}
+      ]
     }
   })
 }
