cb776db63e78038c57743b461bdd2ff9672b3a12
Stone C. Lasley <stonelasley@gmail.com>
2016-01-16 18:11:40 -0700

    emit and broadcast call listener functions

--- src/scope.js
+++ src/scope.js
@@ -48,6 +48,15 @@ Scope.prototype.$beginPhase = function (phase) {
   this.$$phase = phase;
 };
 
+Scope.prototype.$broadcast = function (eventName) {
+
+  var listeners = this.$$listeners[eventName] || [];
+
+  _.forEach(listeners, function (listener) {
+    listener();
+  });
+};
+
 Scope.prototype.$clearPhase = function () {
   this.$$phase = null;
 };
@@ -103,6 +112,15 @@ Scope.prototype.$digest = function () {
       console.error(e);
     }
   }
+};
+
+Scope.prototype.$emit = function (eventName) {
+
+  var listeners = this.$$listeners[eventName] || [];
+
+  _.forEach(listeners, function (listener) {
+    listener();
+  });
 };
 
 Scope.prototype.$eval = function (expr, locals) {
