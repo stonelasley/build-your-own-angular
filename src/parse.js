78e17b8a235c0150898855a33c7906bae2ace1de
Stone C. Lasley <stonelasley@gmail.com>
2016-01-17 17:09:06 -0700

    Can parse floating point numbers

--- src/parse.js
+++ src/parse.js
@@ -82,15 +82,26 @@ Lexer.prototype.lex = function (text) {
   this.tokens = [];
 
   while (this.index < this.text.length) {
+
     this.ch = this.text.charAt(this.index);
-    if (this.isNumber(this.ch)) {
+    if (this.isNumber(this.ch) ||
+      (this.ch === '.' && this.isNumber(this.peek()))) {
+
       this.readNumber();
     } else {
+      
       throw 'Unexpected next character: ' + this.ch;
     }
   }
 
   return this.tokens;
+};
+
+Lexer.prototype.peek = function () {
+
+  return this.index < this.text.length -1 ?
+    this.text.charAt(this.index + 1) :
+    false;
 };
 
 Lexer.prototype.readNumber = function () {
@@ -100,7 +111,7 @@ Lexer.prototype.readNumber = function () {
   while (this.index < this.text.length) {
 
     var ch = this.text.charAt(this.index);
-    if (this.isNumber(ch)) {
+    if (ch === '.' || this.isNumber(ch)) {
 
       number += ch;
     } else {
