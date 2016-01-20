d867a585d0788b4132fff59b1c1408fd57adc9f2
Stone C. Lasley <stonelasley@gmail.com>
2016-01-19 20:57:55 -0700

    parse will throw exception if invalid unicode escape in string

--- test/parse_spec.js
+++ test/parse_spec.js
@@ -99,4 +99,9 @@ describe('parse', function () {
     var fn = parse('"\\u00A0"');
     expect(fn()).toEqual('\u00A0');
   });
+
+  it('will not parse a string with invalid unicode escapes', function () {
+
+    expect(function() {parse('"\\u00T0"'); }).toThrow();
+  });
 });
