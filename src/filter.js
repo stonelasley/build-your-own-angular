d334b195899c3d46087b6c608d9126b06950f1bc
Stone C. Lasley <stonelasley@gmail.com>
2016-02-06 16:42:49 -0700

    allows registering multiple filters

--- src/filter.js
+++ src/filter.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _ = require('lodash');
+
 var filters = {};
 
 function filter(name) {
@@ -9,10 +11,19 @@ function filter(name) {
 
 function register(name, factory) {
 
-  var filter = factory();
-  filters[name] = filter;
+  if (_.isObject(name)) {
 
-  return filter;
+    return _.map(name, function (factory, name) {
+
+      return register(name, factory);
+    });
+  }
+  else {
+    var filter = factory();
+    filters[name] = filter;
+
+    return filter;
+  }
 }
 
 module.exports = {register: register, filter: filter};
--- test/filter_spec.js
+++ test/filter_spec.js
@@ -21,4 +21,28 @@ describe('filter', function () {
     register('my', myFilterFactory);
     expect(filter('my')).toBe(myFilter);
   });
+
+  it('allows registering multiple filters with an object', function () {
+
+    var myFilter = function () {
+    };
+    var myOtherFilter = function () {
+    };
+
+    register({
+
+      my: function () {
+
+        return myFilter;
+      },
+      myOther: function () {
+
+        return myOtherFilter;
+      }
+    });
+
+    expect(filter('my')).toBe(myFilter);
+    expect(filter('myOther')).toBe(myOtherFilter)
+  });
+
 });
